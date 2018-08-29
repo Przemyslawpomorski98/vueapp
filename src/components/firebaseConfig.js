9a0ef790dd5de3d787f0f1b3e5346ffa23105bc9
Przemek <pomorskip2@gmail.com>
2018-08-29 15:18:38 +0200

    vue

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+	apiKey: "AIzaSyAmkuA_LJjF7jbBon5wvjJNePd617HtpiY",
+    authDomain: "vueapp-fd687.firebaseapp.com",
+    databaseURL: "https://vueapp-fd687.firebaseio.com",
+    projectId: "vueapp-fd687",
+    storageBucket: "vueapp-fd687.appspot.com",
+    messagingSenderId: "412256577593"
+}
